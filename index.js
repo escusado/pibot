f9ff80c12054109dbe5b98ed940559b4175cc623
escusado <escusado@gmail.com>
2022-06-24 18:42:16 -0500

    adds cd motor instructions

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 console.log('🤖🍓');
 
-var motorHat = require('motor-hat')({ steppers: [{ W1: 'M1', W2: 'M2' }] }).init();
-motorHat.steppers[0].setSpeed({ pps: 100 });
-motorHat.steppers[0].step('back', 2048, (err, result) => {
-    if (err) return console.log('Oh no, there was an error', err);
-    console.log(`Did ${result.steps} steps ${result.dir} in ${result.duration / 1000} seconds. I had to retry ${result.retried} steps because you set me up quicker than your poor board can handle.`);
-});
-
+var motorHat = require('motor-hat')().init();
+motorHat.dcs[0].runSync('fwd');
+// Set DC motor speed to 50%
+motorHat.dcs[0].setSpeedSync(50);
+// reverse the dc motor to back direction
+motorHat.dcs[0].runSync('back');
+// stop the dc motor
+motorHat.dcs[0].stopSync();
 
 console.log(';🍕');
