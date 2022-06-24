728d444001ee7d7119ed68f55675fb47fdee88bb
escusado <escusado@gmail.com>
2022-06-24 18:50:53 -0500

    adds address

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ var motorHat = require('motor-hat')({
 async function main() {
     motorHat.dcs[0].setSpeedSync(50);
     motorHat.dcs[0].runSync('fwd');
-    await sleep(1000);
     // Set DC motor speed to 50%
     // reverse the dc motor to back direction
     motorHat.dcs[0].runSync('back');
@@ -19,3 +18,25 @@ async function main() {
 }
 
 main();
+
+var stdin = process.stdin;
+
+// without this, we would only get streams once enter is pressed
+stdin.setRawMode(true);
+
+// resume stdin in the parent process (node app won't quit all by itself
+// unless an error or process.exit() happens)
+stdin.resume();
+
+// i don't want binary, do you?
+stdin.setEncoding('utf8');
+
+// on any data into stdin
+stdin.on('data', function (key) {
+    // ctrl-c ( end of text )
+    if (key === '\u0003') {
+        process.exit();
+    }
+    // write the key to stdout all normal like
+    process.stdout.write(key);
+});
