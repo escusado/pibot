209883416c33f9adba8264a8c8ad24bc8f27c3fb
escusado <escusado@gmail.com>
2022-06-24 20:10:39 -0500

    adds oled

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ var motorHat = require('motor-hat')({
     dcs: ['M1', 'M2', 'M3', 'M4'],
 }).init();
 
+var i2c = require('i2c-bus'),
+    i2cBus = i2c.openSync(1),
+    oled = require('oled-i2c-bus');
+
+var opts = {
+    width: 128,
+    height: 32,
+    address: 0x3c
+};
+
+var oled = new oled(i2cBus, opts);
+oled.clearDisplay();
+
+var font = require('oled-font-5x7');
+
+// sets cursor to x = 1, y = 1
+oled.setCursor(1, 1);
+oled.writeString(font, 1, 'Cats and dogs are really cool animals, you know.', 1, true);
+
+
 process.stdin.setRawMode(true);
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
