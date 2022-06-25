90a9cde1cbb411503842a7c178f6f645eafbc505
escusado <escusado@gmail.com>
2022-06-24 19:02:12 -0500

    adds basics controls

--- index.js
+++ index.js
@@ -5,38 +5,47 @@ var motorHat = require('motor-hat')({
     dcs: ['M1', 'M2', 'M3', 'M4'],
 }).init();
 
+process.stdin.setRawMode(true);
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function (key) {
 
-async function main() {
-    motorHat.dcs[0].setSpeedSync(50);
-    motorHat.dcs[0].runSync('fwd');
-    // Set DC motor speed to 50%
-    // reverse the dc motor to back direction
-    motorHat.dcs[0].runSync('back');
-    // stop the dc motor
-    motorHat.dcs[0].stopSync();
-    console.log('🤖');
-}
-
-main();
-
-var stdin = process.stdin;
-
-// without this, we would only get streams once enter is pressed
-stdin.setRawMode(true);
-
-// resume stdin in the parent process (node app won't quit all by itself
-// unless an error or process.exit() happens)
-stdin.resume();
-
-// i don't want binary, do you?
-stdin.setEncoding('utf8');
-
-// on any data into stdin
-stdin.on('data', function (key) {
-    // ctrl-c ( end of text )
     if (key === '\u0003') {
         process.exit();
     }
-    // write the key to stdout all normal like
-    process.stdout.write(key);
+
+    console.log('🚀', key);
+
+    switch (key) {
+        case 's': stop(); break;
+        case 'f': forward(); break;
+        case 'r': reverse(); break;
+        case 'l': left(); break;
+        case 'r': right(); break;
+        default: break;
+    }
 });
+
+function stop() {
+    motorHat.dcs.stopSync();
+}
+
+function forward() {
+    motorHat.dcs.setSpeedSync(50);
+    motorHat.dcs.runSync('fwd');
+};
+
+function reverse() {
+    motorHat.dcs.setSpeedSync(50);
+    motorHat.dcs.runSync('back');
+};
+
+function left() {
+    motorHat.dcs.setSpeedSync(50);
+    motorHat.dcs.runSync('fwd');
+};
+
+function right() {
+    motorHat.dcs.setSpeedSync(50);
+    motorHat.dcs.runSync('fwd');
+};
