910559199bed65d3d865bdd7379d272fb92e16db
escusado <escusado@gmail.com>
2022-06-25 20:27:51 -0500

    adds joystick command

--- joy.js
+++ joy.js
@@ -11,11 +11,11 @@ child.stdout.on('data', (data) => {
     if (jsTestOutput.indexOf('Axes') === 0) {
         const axesString = jsTestOutput.match(/(?<=Axes: ).*(?= Buttons)/);
         const axesParsed = axesString[0].replace(/: /g, ":").split(' ');
-        console.log('axes✚', axesParsed);
+        console.table('axes✚', axesParsed);
 
         const buttonsString = jsTestOutput.match(/(?<=Buttons: ).*$/);
         const buttonsParsed = buttonsString[0].replace(/: /g, ":").split(' ');
-        console.log('buttons🔴', buttonsParsed);
+        console.table('buttons🔴', buttonsParsed);
     }
 });
 
