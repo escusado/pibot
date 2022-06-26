cba055c817d21f764d3324afc114c23128472626
escusado <escusado@gmail.com>
2022-06-25 19:39:15 -0500

    adds joystick command

---
+++ joy.js
@@ -0,0 +1,13 @@
+const { exec } = require("child_process");
+
+exec("jstest /dev/input/js0", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
