79bf46b278111ea9f9417797bfc085effe323a41
escusado <escusado@gmail.com>
2022-06-28 16:55:38 -0500

    display ip

--- index.js
+++ index.js
@@ -98,5 +98,5 @@ function right() {
 
 
 const { dualsenseStore } = require('./joy');
-dualsenseStore.pipe(select((state) => state.axes))
+dualsenseStore.pipe(select((state) => state.axes.dpad))
     .subscribe((data) => { console.log('dpad', data) });
