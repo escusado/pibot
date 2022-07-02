cce6a791a2f3bdd14e9b6337448f5d5c2af80119
JBH <escusado@gmail.com>
2022-07-02 14:33:56 -0500

    adds better state management

--- src/components/jstest-dualsense.js
+++ src/components/jstest-dualsense.js
@@ -41,9 +41,12 @@ class JstestDualsense {
 
   setup() {
     // catch button press or keydown events (note it will NOT detect keyup)
-    spawn("jstest", ["/dev/input/js0"]).stdout.on(
-      "data",
-      (output) => this.jstestOutputStore.update({output: String(output).replace(/^\s+|\s+$|\s+(?=\s)/g, "")})
+    spawn("jstest", ["/dev/input/js0"]).stdout.on("data", (output) =>
+      jsTestOutput.indexOf("Axes") === 0
+        ? this.jstestOutputStore.update({
+            output: String(output).replace(/^\s+|\s+$|\s+(?=\s)/g, ""),
+          })
+        : ""
     );
 
     // react to program output
@@ -53,52 +56,49 @@ class JstestDualsense {
   }
 
   handleJstestOuput(data) {
-    let jsTestOutput = ;
+    console.log(">>>>>>> 🍕", data);
+    //parse axes
+    // const axesString = jsTestOutput.match(/(?<=Axes: ).*(?= Buttons)/);
+    // const axesSliced = axesString[0].replace(/: /g, ":").split(" ");
+    // const newAxes = {
+    //   joyl: {
+    //     x: parseInt(axesSliced[0].split(":")[1]),
+    //     y: parseInt(axesSliced[1].split(":")[1]) * -1,
+    //   },
+    //   joyr: {
+    //     x: parseInt(axesSliced[3].split(":")[1]),
+    //     y: parseInt(axesSliced[4].split(":")[1]) * -1,
+    //   },
+    //   dpad: {
+    //     x: parseInt(axesSliced[6].split(":")[1]),
+    //     y: parseInt(axesSliced[7].split(":")[1]) * -1,
+    //   },
+    // };
 
-    if (jsTestOutput.indexOf("Axes") === 0) {
-      //parse axes
-      const axesString = jsTestOutput.match(/(?<=Axes: ).*(?= Buttons)/);
-      const axesSliced = axesString[0].replace(/: /g, ":").split(" ");
-      const newAxes = {
-        joyl: {
-          x: parseInt(axesSliced[0].split(":")[1]),
-          y: parseInt(axesSliced[1].split(":")[1]) * -1,
-        },
-        joyr: {
-          x: parseInt(axesSliced[3].split(":")[1]),
-          y: parseInt(axesSliced[4].split(":")[1]) * -1,
-        },
-        dpad: {
-          x: parseInt(axesSliced[6].split(":")[1]),
-          y: parseInt(axesSliced[7].split(":")[1]) * -1,
-        },
-      };
+    // //parse buttons
+    // const buttonsString = jsTestOutput.match(/(?<=Buttons: ).*$/);
+    // const buttonsSliced = buttonsString[0].replace(/: /g, ":").split(" ");
+    // const newButtons = {
+    //   cross: buttonsSliced[0].indexOf("on") > 0,
+    //   circle: buttonsSliced[1].indexOf("on") > 0,
+    //   triangle: buttonsSliced[2].indexOf("on") > 0,
+    //   square: buttonsSliced[3].indexOf("on") > 0,
+    //   l1: buttonsSliced[4].indexOf("on") > 0,
+    //   r1: buttonsSliced[5].indexOf("on") > 0,
+    //   l2: buttonsSliced[6].indexOf("on") > 0,
+    //   r2: buttonsSliced[7].indexOf("on") > 0,
+    //   share: buttonsSliced[8].indexOf("on") > 0,
+    //   hamburger: buttonsSliced[9].indexOf("on") > 0,
+    //   ps: buttonsSliced[10].indexOf("on") > 0,
+    //   joyl: buttonsSliced[11].indexOf("on") > 0,
+    //   joyr: buttonsSliced[12].indexOf("on") > 0,
+    // };
 
-      //parse buttons
-      const buttonsString = jsTestOutput.match(/(?<=Buttons: ).*$/);
-      const buttonsSliced = buttonsString[0].replace(/: /g, ":").split(" ");
-      const newButtons = {
-        cross: buttonsSliced[0].indexOf("on") > 0,
-        circle: buttonsSliced[1].indexOf("on") > 0,
-        triangle: buttonsSliced[2].indexOf("on") > 0,
-        square: buttonsSliced[3].indexOf("on") > 0,
-        l1: buttonsSliced[4].indexOf("on") > 0,
-        r1: buttonsSliced[5].indexOf("on") > 0,
-        l2: buttonsSliced[6].indexOf("on") > 0,
-        r2: buttonsSliced[7].indexOf("on") > 0,
-        share: buttonsSliced[8].indexOf("on") > 0,
-        hamburger: buttonsSliced[9].indexOf("on") > 0,
-        ps: buttonsSliced[10].indexOf("on") > 0,
-        joyl: buttonsSliced[11].indexOf("on") > 0,
-        joyr: buttonsSliced[12].indexOf("on") > 0,
-      };
-
-      //update store api
-      this.store.update(() => ({
-        axes: JSON.stringify(newAxes),
-        buttons: JSON.stringify(newButtons),
-      }));
-    }
+    // //update store api
+    // this.store.update(() => ({
+    //   axes: JSON.stringify(newAxes),
+    //   buttons: JSON.stringify(newButtons),
+    // }));
   }
 
   bind(eventName, cb) {
