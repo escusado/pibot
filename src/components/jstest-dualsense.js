dc84652a55cf8dd1464a63474320655b38be7642
JBH <escusado@gmail.com>
2022-07-02 15:06:48 -0500

    update dualsense parser

--- src/components/jstest-dualsense.js
+++ src/components/jstest-dualsense.js
@@ -2,6 +2,9 @@
  * @module JstestDualsense
  * @description Used to read the output of jstest-gtk commandline tool, this
  * needs to be installed (read README setup (TBD)) and connected using bluetooth
+ * as reported by Jstest the dualsense reads as:
+ * Joystick (Wireless Controller) has 8 axes (X, Y, Z, Rx, Ry, Rz, Hat0X, Hat0Y)
+ * and 13 buttons (BtnA, BtnB, BtnX, BtnY, BtnTL, BtnTR, BtnTL2, BtnTR2, BtnSelect, BtnStart, BtnMode, BtnThumbL, BtnThumbR).
  */
 
 const os = require("os");
@@ -15,26 +18,32 @@ class JstestDualsense {
     this.jstestOutputStore = createStore({ withProps: { output: "" } });
     this.store = createStore({
       withProps: {
-        axes: {
-          joyl: { x: 0, y: 0 },
-          joyr: { x: 0, y: 0 },
-          dpad: { x: 0, y: 0 },
-        },
-        buttons: {
-          cross: 0,
-          circle: 0,
-          triangle: 0,
-          square: 0,
-          l1: 0,
-          r1: 0,
-          l2: 0,
-          r2: 0,
-          share: 0,
-          hamburger: 0,
-          ps: 0,
-          joyl: 0,
-          joyr: 0,
-        },
+        joylx: 0,
+        joyly: 0,
+
+        joyrx: 0,
+        joyry: 0,
+
+        dpadx: 0,
+        dpady: 0,
+
+        cross: 0,
+        circle: 0,
+        triangle: 0,
+        square: 0,
+
+        l1: 0,
+        r1: 0,
+        l2: 0,
+        r2: 0,
+        // pressing the sticks
+        l3: 0,
+        r3: 0,
+
+        share: 0,
+        hamburger: 0,
+
+        ps: 0,
       },
     });
   }
@@ -46,33 +55,36 @@ class JstestDualsense {
       .subscribe(this.handleJstestOuput.bind(this));
 
     // catch button press or keydown events (note it will NOT detect keyup)
-    spawn("jstest", ["/dev/input/js0"]).stdout.on("data", (output) =>
-      // wait for output that starts with "A"
-      this.jstestOutputStore.update(() => ({
-        output: String(output).replace(/^\s+|\s+$|\s+(?=\s)/g, ""),
-      }))
+    spawn("jstest", ["/dev/input/js0"]).stdout.on(
+      "data",
+      (output) =>
+        output[0] === 13 && // wait for output that starts with "A"
+        this.jstestOutputStore.update(() => ({
+          output: String(output).replace(/^\s+|\s+$|\s+(?=\s)/g, ""),
+        }))
     );
   }
 
   handleJstestOuput(data) {
     console.log(">>>>>>> 🍉", data);
     //parse axes
-    // const axesString = jsTestOutput.match(/(?<=Axes: ).*(?= Buttons)/);
-    // const axesSliced = axesString[0].replace(/: /g, ":").split(" ");
-    // const newAxes = {
-    //   joyl: {
-    //     x: parseInt(axesSliced[0].split(":")[1]),
-    //     y: parseInt(axesSliced[1].split(":")[1]) * -1,
-    //   },
-    //   joyr: {
-    //     x: parseInt(axesSliced[3].split(":")[1]),
-    //     y: parseInt(axesSliced[4].split(":")[1]) * -1,
-    //   },
-    //   dpad: {
-    //     x: parseInt(axesSliced[6].split(":")[1]),
-    //     y: parseInt(axesSliced[7].split(":")[1]) * -1,
-    //   },
-    // };
+    const axesString = data.match(/(?<=Axes: ).*(?= Buttons)/);
+    const axesSliced = axesString[0].replace(/: /g, ":").split(" ");
+    const newAxes = {
+      joyl: {
+        x: parseInt(axesSliced[0].split(":")[1]),
+        y: parseInt(axesSliced[1].split(":")[1]) * -1,
+      },
+      joyr: {
+        x: parseInt(axesSliced[3].split(":")[1]),
+        y: parseInt(axesSliced[4].split(":")[1]) * -1,
+      },
+      dpad: {
+        x: parseInt(axesSliced[6].split(":")[1]),
+        y: parseInt(axesSliced[7].split(":")[1]) * -1,
+      },
+    };
+    console.log(">>>>>>> 💎", newAxes);
 
     // //parse buttons
     // const buttonsString = jsTestOutput.match(/(?<=Buttons: ).*$/);
