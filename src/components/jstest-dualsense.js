60e7e40182743b13fe6b63910b0873c18f2bab28
JBH <escusado@gmail.com>
2022-07-02 15:18:19 -0500

    update dualsense parser

--- src/components/jstest-dualsense.js
+++ src/components/jstest-dualsense.js
@@ -11,40 +11,42 @@ const os = require("os");
 const { spawn } = require("child_process");
 const { createStore, select } = require("@ngneat/elf");
 
+const DEFAULT_BUTTONS = {
+  joylx: 0,
+  joyly: 0,
+
+  joyrx: 0,
+  joyry: 0,
+
+  dpadx: 0,
+  dpady: 0,
+
+  cross: 0,
+  circle: 0,
+  triangle: 0,
+  square: 0,
+
+  l1: 0,
+  r1: 0,
+  l2: 0,
+  r2: 0,
+  // pressing the sticks
+  l3: 0,
+  r3: 0,
+
+  share: 0,
+  hamburger: 0,
+
+  ps: 0,
+};
+
 class JstestDualsense {
   constructor() {
     console.log("🕹 JstestDualsense");
 
     this.jstestOutputStore = createStore({ withProps: { output: "" } });
     this.store = createStore({
-      withProps: {
-        joylx: 0,
-        joyly: 0,
-
-        joyrx: 0,
-        joyry: 0,
-
-        dpadx: 0,
-        dpady: 0,
-
-        cross: 0,
-        circle: 0,
-        triangle: 0,
-        square: 0,
-
-        l1: 0,
-        r1: 0,
-        l2: 0,
-        r2: 0,
-        // pressing the sticks
-        l3: 0,
-        r3: 0,
-
-        share: 0,
-        hamburger: 0,
-
-        ps: 0,
-      },
+      withProps: DEFAULT_BUTTONS,
     });
   }
 
@@ -67,59 +69,58 @@ class JstestDualsense {
 
   handleJstestOuput(data) {
     if (data) {
-      console.log(">>>>>>> 🍉", data);
       //parse axes
       const axesString = data.match(/(?<=Axes: ).*(?= Buttons)/);
       const axesSliced = axesString[0].replace(/: /g, ":").split(" ");
       const newAxes = {
-        joyl: {
-          x: parseInt(axesSliced[0].split(":")[1]),
-          y: parseInt(axesSliced[1].split(":")[1]) * -1,
-        },
-        joyr: {
-          x: parseInt(axesSliced[3].split(":")[1]),
-          y: parseInt(axesSliced[4].split(":")[1]) * -1,
-        },
-        dpad: {
-          x: parseInt(axesSliced[6].split(":")[1]),
-          y: parseInt(axesSliced[7].split(":")[1]) * -1,
-        },
+        joylx: parseInt(axesSliced[0].split(":")[1]),
+        joyly: parseInt(axesSliced[1].split(":")[1]) * -1,
+
+        joyrx: parseInt(axesSliced[3].split(":")[1]),
+        joyry: parseInt(axesSliced[4].split(":")[1]) * -1,
+
+        dpadx: parseInt(axesSliced[6].split(":")[1]),
+        dpady: parseInt(axesSliced[7].split(":")[1]) * -1,
       };
-      console.log(">>>>>>> 💎", newAxes);
 
       // //parse buttons
-      // const buttonsString = jsTestOutput.match(/(?<=Buttons: ).*$/);
-      // const buttonsSliced = buttonsString[0].replace(/: /g, ":").split(" ");
-      // const newButtons = {
-      //   cross: buttonsSliced[0].indexOf("on") > 0,
-      //   circle: buttonsSliced[1].indexOf("on") > 0,
-      //   triangle: buttonsSliced[2].indexOf("on") > 0,
-      //   square: buttonsSliced[3].indexOf("on") > 0,
-      //   l1: buttonsSliced[4].indexOf("on") > 0,
-      //   r1: buttonsSliced[5].indexOf("on") > 0,
-      //   l2: buttonsSliced[6].indexOf("on") > 0,
-      //   r2: buttonsSliced[7].indexOf("on") > 0,
-      //   share: buttonsSliced[8].indexOf("on") > 0,
-      //   hamburger: buttonsSliced[9].indexOf("on") > 0,
-      //   ps: buttonsSliced[10].indexOf("on") > 0,
-      //   joyl: buttonsSliced[11].indexOf("on") > 0,
-      //   joyr: buttonsSliced[12].indexOf("on") > 0,
-      // };
+      const buttonsString = jsTestOutput.match(/(?<=Buttons: ).*$/);
+      const buttonsSliced = buttonsString[0].replace(/: /g, ":").split(" ");
+      const newButtons = {
+        cross: buttonsSliced[0].indexOf("on") > 0,
+        circle: buttonsSliced[1].indexOf("on") > 0,
+        triangle: buttonsSliced[2].indexOf("on") > 0,
+        square: buttonsSliced[3].indexOf("on") > 0,
+        l1: buttonsSliced[4].indexOf("on") > 0,
+        r1: buttonsSliced[5].indexOf("on") > 0,
+        l2: buttonsSliced[6].indexOf("on") > 0,
+        r2: buttonsSliced[7].indexOf("on") > 0,
+        l3: buttonsSliced[11].indexOf("on") > 0,
+        r3: buttonsSliced[12].indexOf("on") > 0,
+        share: buttonsSliced[8].indexOf("on") > 0,
+        hamburger: buttonsSliced[9].indexOf("on") > 0,
+        ps: buttonsSliced[10].indexOf("on") > 0,
+      };
 
       // //update store api
-      // this.store.update(() => ({
-      //   axes: JSON.stringify(newAxes),
-      //   buttons: JSON.stringify(newButtons),
-      // }));
+      this.store.update(() => ({
+        ...newAxes,
+        ...newButtons,
+      }));
     }
   }
 
-  bind(eventName, cb) {
-    if (eventName === "axes" || eventName === "buttons") {
+  bind(inputName, cb) {
+    if (Object.keys(DEFAULT_BUTTONS).includes(inputName)) {
       this.store.pipe(select((state) => state[eventName])).subscribe(cb);
       return;
     }
-    console.log("Event not supported: ", eventName);
+    console.log(
+      "Input not found: ",
+      eventName,
+      " use these instead: ",
+      Object.keys(DEFAULT_BUTTONS)
+    );
   }
 }
 
