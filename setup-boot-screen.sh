9adf2ee8164100cf6599676806c069c13761145b
JBH <escusado@gmail.com>
2022-07-17 15:16:43 -0500

    better oled scren boot

--- setup-boot-screen.sh
+++ setup-boot-screen.sh
@@ -1,2 +1,11 @@
-cp ./boot-screen.service /etc/systemd/system/
+#!/bin/bash
+
+set -e
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+cp ./oled-boot-screen.service /etc/systemd/system/
 systemctl enable oled-boot-screen
