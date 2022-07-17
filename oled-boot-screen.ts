8b08e618370fa0fba66ce1cc0deb946befa39642
JBH <escusado@gmail.com>
2022-07-17 15:57:23 -0500

    fix boot screen path

--- oled-boot-screen.ts
+++ oled-boot-screen.ts
@@ -42,7 +42,7 @@ oled.clearDisplay();
 oled.writeString(
   font,
   1,
-  `[-c°w°]c Pibot@:\n>${ssid?.[0].slice(0, 18)}\n>${
+  `[-c°w°]c Pibot@:\n>${ssid?.[0].slice(0, 17)}\n>${
     results.get("wlan0")[0]
   }\nwhat is my purpose?
   `,
