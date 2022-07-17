ad273c77a7bf935c934be4b3473c952d3080d206
JBH <escusado@gmail.com>
2022-07-17 16:28:26 -0500

    adds run script

--- oled-boot-screen.ts
+++ oled-boot-screen.ts
@@ -42,7 +42,9 @@ oled.clearDisplay();
 oled.writeString(
   font,
   1,
-  `[-c°w°]@:\n>${ssid?.[0].slice(0, 19)}\n>${results.get("wlan0")[0]}`,
+  `[-c°w°]@:\n>${ssid?.[0].slice(0, 20)}\n>${
+    results.get("wlan0")[0]
+  }\n[...what is my purpose?]`,
   1,
   true
 );
