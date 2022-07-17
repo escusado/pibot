88e0db29791872c4582c0eda5c852cbc79f4ce42
JBH <escusado@gmail.com>
2022-07-17 16:56:00 -0500

    adds run script

--- run-oled-boot-screen.sh
+++ run-oled-boot-screen.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-### BEGIN INIT INFO
-# Provides:          sample.py
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Start daemon at boot time
-# Description:       Enable service provided by daemon.
-### END INIT INFO
 
-cd /home/toy/code/pibot/
-npm run oled-boot-screen
+/home/toy/code/pibot/node_modules/.bin/ts-node /home/toy/code/pibot/oled-boot-screen.ts
