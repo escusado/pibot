92838859b0dd471fd7d438618dc8caf8a8b1cef5
JBH <escusado@gmail.com>
2022-07-22 18:44:56 -0500

    progress

--- oled-boot-screen.ts
+++ oled-boot-screen.ts
@@ -10,40 +10,46 @@ const oledScreen = new oled(i2cBus, {
   address: 0x3c,
 });
 
-const nets = networkInterfaces();
-const results = new Map();
-
-for (const name of Object.keys(nets)) {
-  const netName = nets[name];
-  if (netName) {
-    for (const net of netName) {
-      const familyV4Value = typeof net.family === "string" ? "IPv4" : 4;
-      if (net.family === familyV4Value && !net.internal) {
-        if (!results.has(name)) {
-          results.set(name, []);
-        }
-        results.set(name, [...results.get(name), net.address]);
-      }
-    }
-  }
-}
-
-const ssid = String(require("child_process").execSync("iwgetid")).match(
-  /(?<=").*(?=")/
-);
-
 oledScreen.clearDisplay();
 oledScreen.turnOnDisplay();
 oledScreen.setCursor(1, 1);
-oledScreen.writeString(font, 1, `[-c°w°]-c Pibot v1`, 1, true);
-setTimeout(() => {
-  oledScreen.setCursor(1, 1);
-  oledScreen.clearDisplay();
-  oledScreen.writeString(
-    font,
-    1,
-    `${ssid?.[0]}: ${results.get("wlan0")[0]}`,
-    1,
-    true
+
+try {
+  const nets = networkInterfaces();
+  const results = new Map();
+
+  for (const name of Object.keys(nets)) {
+    const netName = nets[name];
+    if (netName) {
+      for (const net of netName) {
+        const familyV4Value = typeof net.family === "string" ? "IPv4" : 4;
+        if (net.family === familyV4Value && !net.internal) {
+          if (!results.has(name)) {
+            results.set(name, []);
+          }
+          results.set(name, [...results.get(name), net.address]);
+        }
+      }
+    }
+  }
+
+  const ssid = String(require("child_process").execSync("iwgetid")).match(
+    /(?<=").*(?=")/
   );
-}, 2000);
+
+  oledScreen.writeString(font, 1, `[-c°w°]-c Pibot v1`, 1, true);
+
+  setTimeout(() => {
+    oledScreen.setCursor(1, 1);
+    oledScreen.clearDisplay();
+    oledScreen.writeString(
+      font,
+      1,
+      `${ssid?.[0]}: ${results.get("wlan0")[0]}`,
+      1,
+      true
+    );
+  }, 2000);
+} catch (e) {
+  oledScreen.writeString(font, 1, `Error`, 1, true);
+}
