2e71bb2bb961a20a5a149078788c7065e503e363
JBH <escusado@gmail.com>
2022-07-22 18:48:56 -0500

    progress

--- run-oled-boot-screen.sh
+++ run-oled-boot-screen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/toy/code/pibot/node_modules/.bin/ts-node /home/toy/code/pibot/oled-boot-screen.ts
+/home/toy/code/pibot/node_modules/.bin/ts-node /home/toy/code/pibot/oled-boot-screen.ts >  /home/toy/code/pibot/pibot.log
